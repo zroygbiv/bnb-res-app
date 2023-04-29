202997e3ade53b09b1fd866769f227e2dceeffe7
Zach Roth <zach.roth21@gmail.com>
2023-04-28 17:52:35 -0700

    Update add default data function

--- internal/render/render.go
+++ internal/render/render.go
@@ -22,6 +22,9 @@ func NewTemplates(a *config.AppConfig) {
 }
 
 func AddDefaultData(td *models.TemplateData, r *http.Request) *models.TemplateData {
+	td.Flash = app.Session.PopString(r.Context(), "flash")
+	td.Error = app.Session.PopString(r.Context(), "error")
+	td.Warning = app.Session.PopString(r.Context(), "warning")
 	td.CSRFToken = nosurf.Token(r)
 	return td
 }
@@ -100,59 +103,3 @@ func CreateTemplateCache() (map[string]*template.Template, error) {
 
 	return myCache, nil
 }
-
-// RenderTemplateBasic renders template using html/template
-// func RenderTemplateTest(w http.ResponseWriter, tmpl string) {
-// 	parseTemplate, _ := template.ParseFiles("./templates/"+tmpl, "./templates/base.layout.tmpl")
-// 	err := parseTemplate.Execute(w, nil)
-// 	if err != nil {
-// 		fmt.Println("error parsing template:", err)
-// 		return
-// 	}
-// }
-
-// var tc = make(map[string]*template.Template)
-
-// func RenderTemplateBasic(w http.ResponseWriter, t string) {
-// 	var tmpl *template.Template
-// 	var err error
-
-// 	// check to see if we already have the template in our cache
-// 	_, inMap := tc[t]
-// 	if !inMap {
-// 		// need to create the template
-// 		log.Println("creating template and adding to cache")
-// 		err = createTemplateCache(t)
-// 		if err != nil {
-// 			log.Println(err)
-// 		}
-// 	} else {
-// 		// we have the template in the cache
-// 		log.Println("using cached template")
-// 	}
-
-// 	tmpl = tc[t]
-
-// 	err = tmpl.Execute(w, nil)
-// 	if err != nil {
-// 		log.Println(err)
-// 	}
-// }
-
-// func createTemplateCache(t string) error {
-// 	templates := []string{
-// 		fmt.Sprintf("./templates/%s", t),
-// 		"./templates/base.layout.tmpl",
-// 	}
-
-// 	// parse the template
-// 	tmpl, err := template.ParseFiles(templates...)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	// add template to cache (map)
-// 	tc[t] = tmpl
-
-// 	return nil
-// }
