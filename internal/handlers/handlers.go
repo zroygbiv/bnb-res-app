bdd0678386ee01a22aa16bf4007a05c6c111af7c
Zach Roth <zach.roth21@gmail.com>
2023-04-28 17:51:08 -0700

    Update reservation handlers

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/zroygbiv/bnb-res-app/internal/config"
@@ -38,7 +39,6 @@ func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, r, "home.page.tmpl", &models.TemplateData{})
 }
 
-
 // About is the about page handler
 func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	// perform logic
@@ -54,29 +54,26 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	})
 }
 
-
 // Kitchen renders kitchen page; displays form
 func (m *Repository) Kitchen(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, r, "kitchen.page.tmpl", &models.TemplateData{})
 }
-
 
 // Common renders common page; displays form
 func (m *Repository) Common(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, r,"common.page.tmpl", &models.TemplateData{})
 }
 
-
 // Bedroom renders bedroom page; displays form
 func (m *Repository) Bedroom(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, r, "bedroom.page.tmpl", &models.TemplateData{})
 }
 
-
 // Availability renders search availability page; displays form
 func (m *Repository) Availability(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, r, "search-availability.page.tmpl", &models.TemplateData{})
 }
+
 // PostAvailability renders search availability page; displays form
 func (m *Repository) PostAvailability(w http.ResponseWriter, r *http.Request) {
 	start := r.Form.Get("start")
@@ -84,20 +81,79 @@ func (m *Repository) PostAvailability(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(fmt.Sprintf("Start date is %s and end date is %s", start, end)))
 }
 
-
 // Reservation renders reservation page; displays form
 func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
+	var emptyReservation models.Reservation
+	data := make(map[string]interface{})
+	data["reservation"] = emptyReservation
+
 	render.RenderTemplate(w, r, "make-reservation.page.tmpl", &models.TemplateData{
 		Form: forms.New(nil),
+		Data: data,
 	})
 }
+
 // PostReservation handles the posting of a reservation form
 func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
+	
+	// Parse the form
+	err := r.ParseForm()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	// Create reservation
+	reservation := models.Reservation{
+		FirstName: r.Form.Get("first_name"),
+		LastName: r.Form.Get("last_name"),
+		Phone: r.Form.Get("phone"),
+		Email: r.Form.Get("email"),
+	}
+
+	form := forms.New(r.PostForm)
+
+	form.Required("first_name", "last_name", "email")
+	form.MinLength("first_name", 2, r)
+	form.MinLength("last_name", 2, r)
+	form.IsEmail("email")
+
+	if !form.Valid() {
+		data := make(map[string]interface{})
+		data["reservation"] = reservation
+
+		render.RenderTemplate(w, r, "make-reservation.page.tmpl", &models.TemplateData{
+			Form: form,
+			Data: data,
+		})
+		return
+	}
+
+	m.App.Session.Put(r.Context(), "reservation", reservation)
+
+	http.Redirect(w, r, "/reservation-summary", http.StatusSeeOther)
 
 }
-
 
 // Reservation renders reservation page; displays form
 func (m *Repository) Contact(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, r, "contact.page.tmpl", &models.TemplateData{})
+}
+
+func (m *Repository) ReservationSummary(w http.ResponseWriter, r *http.Request) {
+	reservation, ok := m.App.Session.Get(r.Context(), "reservation").(models.Reservation)
+	if !ok {
+		log.Println("Can't get item from session...")
+		m.App.Session.Put(r.Context(), "error", "Can't get reservation from session...")
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+	m.App.Session.Remove(r.Context(), "reservation")
+	
+	data := make(map[string]interface{})
+	data["reservation"] = reservation
+
+	render.RenderTemplate(w, r, "reservation-summary.page.tmpl", &models.TemplateData{
+		Data: data,
+	})
 }
